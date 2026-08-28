0a7fc4b00970246a32d0aa88a5a1217b7d894fb7
emryll <emryll5@proton.me>
2026-08-28 17:20:23 +0300

    feat: rank process handle count

---
+++ analysis.go
@@ -0,0 +1,24 @@
+package main
+
+import "sort"
+
+// Get a list of all active processes
+// sorted in descending order of handle count.
+func RankProcessHandleCount() []*Process {
+	var processes []*Process
+
+	g_ProcessTable.mu.RLock()
+	defer g_ProcessTable.mu.RUnlock()
+
+	for _, ps := range g_ProcessTable.Table {
+		processes = append(processes, ps)
+	}
+	sort.Slice(processes, func(i, j int) bool {
+		return processes[i].HandleCount > processes[j].HandleCount
+	})
+
+	return processes
+}
+
+//TODO GetClusterStatistics
+//TODO FindMostWideReaching
