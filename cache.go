4ae8d9621c4af50773df76c81582d3fcae363f0c
emryll <emryll5@proton.me>
2026-08-28 13:58:46 +0300

    feat: core of cache cleanup

--- cache.go
+++ cache.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"math"
+	"sort"
 	"sync"
 	"time"
 )
@@ -118,4 +121,109 @@ func (c *HandleCache) getPsHandleCountsByType(pid uint32) map[string]int {
 		}
 	}
 	return results
+}
+
+//*====================================[ Cache Cleanup ]=========================================
+
+// Get the base priority score for the given object.
+// Used to calculate priority for score based handle cache cleanup.
+func (handle *HandleEntry) GetObjectScore() int {
+	var tier int
+	if val, exists := ObjectTypeTier[handle.Type]; exists {
+		tier = val
+	}
+	switch tier {
+	case 1:
+		return HCC_OBJECT_TIER_1_SCORE
+	case 2:
+		return HCC_OBJECT_TIER_2_SCORE
+	case 3:
+		return HCC_OBJECT_TIER_3_SCORE
+	case 4:
+		return HCC_OBJECT_TIER_4_SCORE
+	}
+	return HCC_OBJECT_UNKNOWN_SCORE
+}
+
+// Calculate the final priority of a handle entry.
+// Priority is based on age of handle and base priority of object.
+// A higher priority means that it should be cleaned up sooner.
+func (handle *HandleEntry) CalculatePriority(stamp ...int64) int {
+	var now int64
+	if len(stamp) == 0 {
+		now = time.Now().UnixMilli()
+	} else {
+		now = stamp[0]
+	}
+	timeElapsed := int(now - handle.LastSeen)
+	timeScore := int(math.Pow(float64(timeElapsed)/1000, HCC_TIME_POWER))
+	timeScore *= HCC_TIME_MULTIPLIER
+
+	objectScore := handle.GetObjectScore()
+	objectScore *= HCC_OBJECT_MULTIPLIER
+
+	// should you do multiplication or addition?
+	return (timeScore + objectScore) * HCC_MULTIPLIER_CONST
+}
+
+func (c *HandleCache) Cleanup(quota ...int) {
+	var cleanupQuota int
+	if len(quota) == 0 {
+		cleanupQuota = HCC_DEFAULT_QUOTA
+	} else {
+		cleanupQuota = quota[0]
+	}
+
+	c.mu.RLock()
+	// helper struct to avoid recalculating priority
+	type handleWithPriority struct {
+		handle   *HandleEntry
+		priority int
+	}
+
+	//* get cache as slice
+	now := time.Now().UnixMilli()
+	handles := make([]handleWithPriority, 0, 5000)
+	for _, objectCache := range c.Cache {
+		for _, entries := range objectCache {
+			for _, handle := range entries {
+				handles = append(handles, handleWithPriority{
+					handle: handle, priority: handle.CalculatePriority(now)})
+			}
+		}
+	}
+	c.mu.RUnlock()
+	//* sort handles in descending priority
+	sort.Slice(handles, func(i, j int) bool {
+		return handles[i].priority > handles[j].priority
+	})
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	//* cleanup highest priority handles until quota is met
+	for i := 0; i < cleanupQuota && i < len(handles); i++ {
+		var (
+			pid        = handles[i].handle.Pid
+			handle     = handles[i].handle.Handle
+			objectType = GetTypeName(handles[i].handle.Type)
+		)
+		// between mutex it couldve been deleted
+		if _, exists := c.Cache[pid]; !exists {
+			continue
+		}
+		if _, exists := c.Cache[pid][objectType]; !exists {
+			continue
+		}
+		// delete the handle entry
+		delete(c.Cache[pid][objectType], handle)
+		if len(c.Cache[pid][objectType]) == 0 {
+			delete(c.Cache[pid], objectType)
+		}
+		if len(c.Cache[pid]) == 0 {
+			delete(c.Cache, pid)
+		}
+	}
+}
+
+func (c *HandleCache) CacheCleaner(wg *sync.WaitGroup, ctx context.Context) {
+	//TODO:
 }
