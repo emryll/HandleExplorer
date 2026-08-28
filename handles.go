6f39f66f5e43db0022997c8146a35a48b8a67f73
emryll <emryll5@proton.me>
2026-08-28 03:38:51 +0300

    feat: convert handle entry from c struct to go

--- handles.go
+++ handles.go
@@ -1,12 +1,20 @@
 package main
 
+//#include "handles.h"
+import "C"
+
 import (
 	"unsafe"
 )
 
 // Get global handle table. Note that this is heavy; in the ballpark of 1000ms+
 func GetGlobalHandleTable() []HandleEntry {
-	//TODO: add silent benchmark
+	hb := GetBenchmarker("HandleTable")
+	if hb != nil {
+		stop := hb.Benchmark()
+		defer stop()
+	}
+
 	var handleCount C.size_t
 	cHandleEntries := C.GetGlobalHandleTable(&handleCount)
 	cSlice := unsafe.Slice((*cHandleEntry)(unsafe.Pointer(cHandleEntries)), int(handleCount))
@@ -26,7 +34,7 @@ func (h HandleEntry) ConvertToAccessEntry() AccessEntry {
 	entry.Pid = h.Pid
 	entry.Params = h.Parameters
 
-	if entry.Object == OBJECT_TYPE_PROCESS {
+	if entry.Object == OBJ_TYPE_PROCESS {
 		pathParam := h.GetParameter("ImagePath")
 		if !pathParam.Empty() {
 			entry.Name = h.Parameters["ImagePath"].GetValue().(string)
@@ -38,5 +46,30 @@ func (h HandleEntry) ConvertToAccessEntry() AccessEntry {
 			entry.Name = GetAnsiValue(nameParam.Buffer)
 		}
 	}
+	return entry
+}
+
+// Convert mirrored C HANDLE_ENTRY layout into go version
+func (h cHandleEntry) GoEntry() HandleEntry {
+	var entry HandleEntry
+	entry.FirstSeen = h.FirstSeen
+	entry.LastSeen = h.LastSeen
+	entry.Handle = h.Handle
+	entry.Access = h.Access
+	entry.Type = h.Type
+	entry.Pid = h.Pid
+
+	if h.ParamsSize == 0 || h.Params == nil || uintptr(h.Params) == ^uintptr(0) {
+		return entry
+	}
+
+	buf := C.GoBytes(unsafe.Pointer(h.Params), C.int(h.ParamsSize))
+	params := ParseParameters(buf)
+	entry.Parameters = make(map[string]Parameter)
+	for _, param := range params {
+		entry.Parameters[param.Name] = param
+	}
+	C.free(unsafe.Pointer(h.Params))
+
 	return entry
 }
