0f5ab02406461b74ec9aa3c9fbc47bbb60ae2875
emryll <emryll5@proton.me>
2026-09-10 17:28:06 +0300

    fix: dependency cycle

--- stats/analysis.go
+++ stats/analysis.go
@@ -1,53 +1,33 @@
 package stats
 
 import (
-	_ "HandleExplorer/app"
-	"sort"
 	"sync"
 )
-
-// Get a list of all active processes
-// sorted in descending order of handle count.
-func RankProcessHandleCount() []*ps.Process {
-	var processes []*ps.Process
-
-	PsTable.mu.RLock()
-	defer PsTable.mu.RUnlock()
-
-	for _, ps := range PsTable.Table {
-		processes = append(processes, ps)
-	}
-	sort.Slice(processes, func(i, j int) bool {
-		return processes[i].GetHandleCount() > processes[j].GetHandleCount()
-	})
-
-	return processes
-}
 
 //TODO FindMostWideReaching
 
 // Cached stats about current state.
 type SessionStats struct {
-	mu                   sync.RWMutex
+	sync.RWMutex
 	TotalActiveHandles   int
 	TotalActiveProcesses int
 }
 
 // Get the total handle count (global).
 // This will read lock the session stats.
-func GetTotalHandleCount() int {
-	RuntimeStats.mu.RLock()
-	defer RuntimeStats.mu.RUnlock()
-	return RuntimeStats.TotalActiveHandles
+func GetTotalHandleCount(rtStats *SessionStats) int {
+	rtStats.RLock()
+	defer rtStats.RUnlock()
+	return rtStats.TotalActiveHandles
 }
 func (s *SessionStats) SetProcessCount(count int) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
+	s.Lock()
+	defer s.Unlock()
 	s.TotalActiveProcesses = count
 }
 
 func (s *SessionStats) SetHandleCount(count int) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
+	s.Lock()
+	defer s.Unlock()
 	s.TotalActiveHandles = count
 }
